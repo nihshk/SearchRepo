6cc92d31b950c2dc50ce049ec8dc3b824afa91f9
Nithesh Shettigar <nihsh.k@gmail.com>
2018-05-23 16:05:40 +0530

    first commit

---
+++ SearchRepo/src/app/services/github.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  private username:string;
+  private client_id = '6788b0b421ab9a2867a0';
+  private client_secret = '8f54bbdfdddea535649deaa5e4912cfbc23ca42c';
+
+  constructor(private _http:Http) {
+    console.log('Github Service Ready...')
+    this.username='nihshk'
+  }
+
+  getUser(){
+    return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'?client_secret='+this.client_secret)
+    .map(res => res.json());
+  }
+}
